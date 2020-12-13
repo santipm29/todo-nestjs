8362b69753fd03e4f4d82efd223365ef094742ef
Santiago Patiño <santipmartinez@outlook.com>
2020-12-13 18:24:06 -0500

    :sparkles: add typeorm config and tasksmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
+import { TypeORMConfig } from './config/typeorm.config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(TypeORMConfig), TasksModule],
 })
 export class AppModule {}
