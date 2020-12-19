ad887d539ec56af8330c35575ea991f52c7e0c3b
Santiago Patiño <santipmartinez@outlook.com>
2020-12-19 10:37:15 -0500

    :sparkles: add jwt

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { UserRepository } from './user.repository';
+import { JwtPayload } from './jwt-payload.interface';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UnauthorizedException } from '@nestjs/common';
+import { User } from './user.entity';
+
+export class JwtEstrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'ieieifejifeife',
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
